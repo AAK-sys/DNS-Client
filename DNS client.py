b0cddbdf9fde60677cf67945398a359ad95c5acb
AAK-sys <85410166+AAK-sys@users.noreply.github.com>
2023-02-23 13:26:49 -0500

    updated parse_response

--- DNS client.py
+++ DNS client.py
@@ -7,7 +7,8 @@ def get_response(URL):
 
 #parse the dns server response
 def parse_response(hexastring):
-    # hexastring = ""
+    # gmu.edu response example. Some websites have multiple IP addresses that the dns resolves hence multiple hexadecimal responses or a longer hexadecimal. How this will affect the parsing is still unkonwn.
+    hexastring = "9aa38580000100010000000003676d75036564750000010001c00c0001000100000e10000481ae861c"
 
 
 url = "gmu.edu"
