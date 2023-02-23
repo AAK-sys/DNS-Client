4c8c3026107b1f6503c69e872d6c50206fe2d080
AAK-sys <85410166+AAK-sys@users.noreply.github.com>
2023-02-23 17:27:52 -0500

    Add files via upload

---
+++ illegal file.py
@@ -0,0 +1,19 @@
+import dns.resolver
+import binascii
+
+domain = 'gmu.edu'
+response = dns.resolver.query(domain, 'A', raise_on_no_answer=False)
+
+if response.rrset is not None:
+    # Obtain the raw response as bytes
+    raw_response = response.response.to_wire()
+
+    # Convert the raw response bytes to hex values
+    hex_response = binascii.hexlify(raw_response)
+
+    # Print the hex values
+    print(hex_response)
+else:
+    print(f"No records found for {domain}")
+
+
