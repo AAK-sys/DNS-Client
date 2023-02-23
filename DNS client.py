6debeaba8ff55a98ebbf93cef3e867303c0a0d0f
AAK-sys <85410166+AAK-sys@users.noreply.github.com>
2023-02-23 12:42:15 -0500

    First Commit

---
+++ DNS client.py
@@ -0,0 +1,16 @@
+import socket
+
+# get response from url
+def get_response(URL):
+
+    return ""
+
+#parse the dns server response
+def parse_response(hexastring):
+    # hexastring = ""
+
+
+url = "gmu.edu"
+parse_response(get_response(url))
+
+
