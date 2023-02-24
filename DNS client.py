7abe9d3788d907a90a87faed752190335d13b8ae
ndeluca <ndeluca@gmu.edu>
2023-02-24 10:19:48 -0500

    bit masked for header bytes

--- DNS client.py
+++ DNS client.py
@@ -41,18 +41,40 @@ def get_response(URL):
 def parse_response(hexastring):
     # gmu.edu response example. Some websites have multiple IP addresses that the dns resolves hence multiple 
     # hexadecimal responses or a longer hexadecimal. How this will affect the parsing is still unknown.
-    hexastring = b's\x05\x81\x80\x00\x01\x00\x01\x00\x00\x00\x00\x03gmu\x03edu\x00\x00\x01\x00\x01\xc0\x0c\x00\x01\x00\x01\x00\x00\x02{\x00\x04\x81\xae\x86\x1c'
-    
+
     # If standard response, process Header, Question, & Answer
 
-    # Processing header
+    # Processing header -----------------------------------------------------------------------------
     header_bytes = []
     for i in range(12):
         header_bytes.append(hexastring[i])
     
     header_id_bytes = []
-    for i in range(2): header_id_bytes.append(header_bytes[i])
+    for i in range(0, 1): header_id_bytes.append(header_bytes[i])
+
+    # Bit masking 1st half of 2nd row of bytes to get qr, opcode, aa, tc, & rd
+    header_qr = header_bytes[2] & 1
+    header_opcode = header_bytes[2] & 30
+    header_aa = header_bytes[2] & 32
+    header_tc = header_bytes[2] & 64
+    header_rd = header_bytes[2] & 128
+    # Bit masking 2nd half of 2nd row of bytes to get ra, z, & rcode
+    header_ra = header_bytes[3] & 1
+    header_z = header_bytes[3] & 14
+    header_rcode = header_bytes[3] & 240
+    
+    # Processing question ---------------------------------------------------------------------------
+    question_bytes = []
+    for i in range(3, 13): question_bytes.append(hexastring[i])
+
+    question_qname_bytes = []
+    question_qtype_bytes = []
+    question_qclass_bytes = []
+    for i in range(0, 5): question_qname_bytes.append(question_bytes[i])
+    for i in range(6, 7): question_qtype_bytes.append(question_bytes[i])
+    for i in range(8, 9): question_qclass_bytes.append(question_bytes[i])
+
 
 
 url = "gmu.edu"
-parse_response(get_response(url))
+parse_response(b'\x05\x81\x80\x00\x01\x00\x01\x00\x00\x00\x00\x03gmu\x03edu\x00\x00\x01\x00\x01\xc0\x0c\x00\x01\x00\x01\x00\x00\x02{\x00\x04\x81\xae\x86\x1c')
