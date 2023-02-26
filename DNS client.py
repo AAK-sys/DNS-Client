7c3695655758e3a64e84fe6aa918fca35416a321
ndeluca <ndeluca@gmu.edu>
2023-02-26 13:28:13 -0500

    Parsed header and question section entries

--- DNS client.py
+++ DNS client.py
@@ -46,35 +46,68 @@ def parse_response(hexastring):
 
     # Processing header -----------------------------------------------------------------------------
     header_bytes = []
-    for i in range(12):
-        header_bytes.append(hexastring[i])
+    for i in range(12): header_bytes.append(hexastring[i])
     
-    header_id_bytes = []
-    for i in range(0, 1): header_id_bytes.append(header_bytes[i])
-
+    header_id = concatBytes(header_bytes[0], header_bytes[1])
     # Bit masking 1st half of 2nd row of bytes to get qr, opcode, aa, tc, & rd
-    header_qr = header_bytes[2] & 1
+    header_qr = 1
     header_opcode = header_bytes[2] & 30
     header_aa = header_bytes[2] & 32
     header_tc = header_bytes[2] & 64
-    header_rd = header_bytes[2] & 128
-    # Bit masking 2nd half of 2nd row of bytes to get ra, z, & rcode
+    header_rd = 0
+    # Bit masking 2nd half of 3rd row of bytes to get ra, z, & rcode
     header_ra = header_bytes[3] & 1
     header_z = header_bytes[3] & 14
     header_rcode = header_bytes[3] & 240
+    header_qd_count = concatBytes(header_bytes[4], header_bytes[5])
+    header_an_count = concatBytes(header_bytes[6], header_bytes[7])
+    header_ns_count = concatBytes(header_bytes[8], header_bytes[9])
+    header_ar_count = concatBytes(header_bytes[10], header_bytes[11])
     
     # Processing question ---------------------------------------------------------------------------
-    question_bytes = []
-    for i in range(3, 13): question_bytes.append(hexastring[i])
+    # Keeps track of where we are in the hexastring
+    pos = 12;
+    # We may have more tha one question, so each one will be in an array
+    questions = []
+    for i in range(0, header_qd_count):
+        question = []
+        domain = []
+        # Getting domain/qname
+        for j in range(3):
+            count_octet = hexastring[pos]
+            part = ""
+            for z in range(count_octet):
+                pos += 1
+                part += chr(hexastring[pos])
+            domain.append(part)
+            pos += 1
+        question.append(domain)
+        # Getting qtype
+        qtype = concatBytes(hexastring[pos], hexastring[pos+1])
+        pos += 2
+        # Getting qclass
+        qclass = concatBytes(hexastring[pos], hexastring[pos+1])
+        pos += 2
+        question.append(qtype)
+        question.append(qclass)
+        questions.append(question)
+    
+    print(questions)
+        
 
-    question_qname_bytes = []
-    question_qtype_bytes = []
-    question_qclass_bytes = []
-    for i in range(0, 5): question_qname_bytes.append(question_bytes[i])
-    for i in range(6, 7): question_qtype_bytes.append(question_bytes[i])
-    for i in range(8, 9): question_qclass_bytes.append(question_bytes[i])
+
+    # print(hexastring[12])
+    # print(chr(hexastring[13]))
+    # print(hexastring[14])
+    # print(hexastring[15])
+    # print(hexastring[16])
+
+# Helper function that concats 2 octets into single 16 bit int, (all bin values must be in int form)
+def concatBytes(x, y):
+    res = x<<8 | y
+    return res
 
 
 
 url = "gmu.edu"
-parse_response(b'\x05\x81\x80\x00\x01\x00\x01\x00\x00\x00\x00\x03gmu\x03edu\x00\x00\x01\x00\x01\xc0\x0c\x00\x01\x00\x01\x00\x00\x02{\x00\x04\x81\xae\x86\x1c')
+parse_response(b'\t\xa6\x81\x80\x00\x01\x00\x01\x00\x00\x00\x00\x03www\x03gmu\x03edu\x00\x00\x01\x00\x01\xc0\x0c\x00\x01\x00\x01\x00\x00\x08\x89\x00\x04\xc0|\xf9D')
